6e901e8055ef764de2bf10ac496938e469d6a28a
Ivan Rendon <ivanrendon@Ivans-iMac.attlocal.net>
2019-05-27 19:08:33 -0400

    renders itmes from database to dom and adds functionality to total order

--- public/js/app.js
+++ public/js/app.js
@@ -1,81 +1,80 @@
-// '.tbl-content' consumed little space for vertical scrollbar, scrollbar width depend on browser/os/platfrom. Here calculate the scollbar width .
+
+// Get the DOM ready ============================================================>>>>>>>>
 $(() => {
     loadProducts();
 });
+// ===========================================================================>>>>>>>>
 
-// Function to render all the products in from the database
+
+// Global Varables ==================================================>>>>>>>>>>>>>>>>>>>>>>>
+var subtotal = 0;
+var total = 0;
+var tax = 0;
+//===========================================================================
+
+
+// Function to render all the products in from the database==============================
 var loadProducts = () => {
     $.get("/products", (products) => {
         $.each(products, (indexOfArr, eachObject) => {
-            // console.log(eachObject)
-            $("#products").append(`<tr>
+
+            $("#products").append(`<tr id="itemrow">
                             <td class="itemId">${eachObject.id}</td>
-                            <td>${eachObject.product_name}</td>
+                            <td class="productName">${eachObject.product_name}</td>
                             <td>${eachObject.department_name}</td>
                             <td>$ ${eachObject.price}</td>
-                            <td>${eachObject.stock_quantity} in Stock</td>
-                            <td><button type="button" class="btn btn-outline-warning" data-item="${eachObject.id}">add</button></td>
                             <td><select class="form-control" id="exampleFormControlSelect1">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                            </select></td></tr>`)
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            </select></td>
+                            <td><button type="button" class="btn btn-outline-warning" data-item="${eachObject.id}"><i class="fas fa-cart-plus"></i></button></td>
+                            </tr>`);
         });
     });
 };
 
+
+
 //function to add to shopping cart from table
 $(document).on("click", ".btn-outline-warning", function () {
+    
     var itemId = $(this).data().item;
-    console.log(itemId)
+    var quantity = $(this).closest("#itemrow").find("select#exampleFormControlSelect1 option:selected").val();
+    console.log(quantity)
+
     $.get("/api/products/" + itemId, function (data, status) {
-        console.log(data);
-        console.log(status);
-        // $(".result").html(data);
-        // alert("Load was performed.");
+        if (data.stock_quantity < 0) {
+            $("#cardBody").html(`<h4>Sold out!</h4>
+            <h4>The item you are trying to buy is unavailable. Don't worry, we are getting more soon!</h4>`);
+        } else {
+            subtotal += data.price * quantity;
+            tax += (data.price * .06) * quantity;
+            total = (tax + subtotal) * quantity;
+            $("#addItems").append(`<tr><td class="qty">${quantity}</td><td>${data.product_name}</td><td class="tprice">$ ${data.price}</td></tr>`);
+            $("#cardBody").html(`<h5 class="card-title text-right">Subtotal&emsp;&emsp;$ ${parseFloat(subtotal).toFixed(2)}</h5>
+                <h5 class="card-title text-right">Tax&emsp;&emsp;$ ${parseFloat(tax).toFixed(2)}</h5>
+                <hr />
+                <h4 class="card-title text-right">Total&emsp;&emsp;$ ${parseFloat(total).toFixed(2)}</h4>
+                <a class="btn btn-outline-warning btn-block" id="submitOrder">Submit Order</a>`);
+        }
     });
-
-
-
 });
 
+// =============== Function to Update database after submit order
+var submitOrder = () => {
+    $(document).on("click", "#submitOrder", () => {
 
-
-/* <tr>
-    <td>AAC</td>
-    <td>AUSTRALIAN COMPANY </td>
-    <td>$1.38</td>
-    <td>+2.01</td>
-    <td>-0.36%</td>
-</tr> */
-// $.get("/products", function (data) {
-//     // var rowsToAdd = [];
-//     // for (var i = 0; i < data.length; i++) {
-//     //     rowsToAdd.push(data[i]);
-//     // }
-//     console.log(data);
-//     // renderAuthorList(rowsToAdd);
-//     // nameInput.val("");
-// });
-
-
-// +++++++++++++++++++++++++ From website ++++++++++++++++++++++
-// var loadProducts = function () {
-//     $.get("/products", function (products) {
-//         $.each(products, function (i, v) {
-//             var row = $("<tr class='itemRow'>");
-//             $("<td class='itemid' data-itemid='" + v.ItemID + "'>").text(v.ItemID).appendTo(row);
-//             $("<td>").text(v.ProductName).appendTo(row);
-//             $("<td>").text(v.DepartmentName).appendTo(row);
-//             $("<td class='cashmoney'>").text(v.Price).appendTo(row);
-//             $(amazonQ + "<button type='button' class='btn btn-default btn-sm addItem'><span class='glyphicon glyphicon-shopping-cart'></span> Shopping Cart</button>").appendTo(row);
-//             row.appendTo("#products");
-//         });
-//         $(".cashmoney").autoNumeric("init", {
-//             aSep: ",",
-//             aSign: "$"
-//         });
-//     });
-// };
+        var jqxhr = $.post("/order?" + $.param(order), function () {
+        })
+            .done(function () {
+                $("#cart").empty();
+                $("#cart-status").html("<h2>Order complete. Thank you!");
+            })
+            .fail(function () {
+                console.log("womp womp.");
+            });
+    });
+}; 
