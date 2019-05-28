493b91f1b07b7246ff0b77996c9a0f3365865b95
Ivan Rendon <ivanrendon@Ivans-iMac.attlocal.net>
2019-05-28 17:02:04 -0400

    clean code

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -10,22 +10,22 @@ var db = require("../models");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
 
-  // GET route for getting all of the Products
-  app.get("/products", function(req, res) {
-    db.Product.findAll({})
-        .then((dbPost) => {
-        res.json(dbPost);
-      });
-  });
-    
-    
-  // PUT route for updating Products
+    // GET route for getting all of the Products
+    app.get("/products", function (req, res) {
+        db.Product.findAll({})
+            .then((dbPost) => {
+                res.json(dbPost);
+            });
+    });
+
+
+    // PUT route for updating Products
     app.post("/api/products/", function (req, res) {
         console.log(req.body);
         var arrOfObjects = req.body;
-        arrOfObjects.forEach(function (element) { 
+        arrOfObjects.forEach(function (element) {
             db.Product.update(
                 {
                     stock_quantity: element.stock_quantity
@@ -35,92 +35,17 @@ module.exports = function(app) {
                     }
                 })
         });
+    });
 
-    // db.Product.update(newData,
-    //   {
-    //     where: {
-    //       id: req.body.id
-    //     }
-    //   })
-    //   .then(function(dbPost) {
-    //     res.json(dbPost);
-    //   });
-  });
-    // app.get('/api/5', (req, res) => {
-    //     let query;
-    //     if (req.params.userId) {
-    //         query = Blog.findAll({
-    //             include: [
-    //                 { model: User, where: { id: req.params.userId } },
-    //                 { model: Tag }
-    //             ]
-    //         })
-    //     } else {
-    //         query = Blog.findAll({ include: [Tag, User] })
-    //     }
-    //     return query.then(blogs => res.json(blogs))
-    // })
-    
-    
-//   Get route for returning posts of a specific category
+    //   Get route for returning Products of a specific category
     app.get("/api/products/:id", function (req, res) {
-        
-      db.Product.findOne({
-      where: {
-        id: req.params.id
-      }
-      }).then(function (singleProduct) {
-          res.json(singleProduct);
-      });
-  });
 
-//   // Get route for retrieving a single post
-//   app.get("/api/posts/:id", function(req, res) {
-//     db.Post.findOne({
-//       where: {
-//         id: req.params.id
-//       }
-//     })
-//       .then(function(dbPost) {
-//         res.json(dbPost);
-//       });
-//   });
-
-//   // POST route for saving a new post
-//   app.post("/api/posts", function(req, res) {
-//     console.log(req.body);
-//     db.Post.create({
-//       title: req.body.title,
-//       body: req.body.body,
-//       category: req.body.category
-//     })
-//       .then(function(dbPost) {
-//         res.json(dbPost);
-//       });
-//   });
-
-//   // DELETE route for deleting posts
-//   app.delete("/api/posts/:id", function(req, res) {
-//     db.Post.destroy({
-//       where: {
-//         id: req.params.id
-//       }
-//     })
-//       .then(function(dbPost) {
-//         res.json(dbPost);
-//       });
-//   });
-
-//   // PUT route for updating posts
-//   app.put("/api/posts", function(req, res) {
-//     db.Post.update(req.body,
-//       {
-//         where: {
-//           id: req.body.id
-//         }
-//       })
-//       .then(function(dbPost) {
-//         res.json(dbPost);
-//       });
-//   });
+        db.Product.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(function (singleProduct) {
+            res.json(singleProduct);
+        });
+    });
 };
--- scripts/seed.js
+++ scripts/seed.js
@@ -1,6 +1,6 @@
 const db = require("../models");
 const products = require("./products.json");
-// const posts = require("./posts.json");
+
 
 db.sequelize.sync({ force: true }).then(function () {
     return db.Product.bulkCreate(products);
@@ -8,8 +8,3 @@ db.sequelize.sync({ force: true }).then(function () {
     db.sequelize.close();
 });
 
-// db.sequelize.sync({ force: true }).then(() => {
-//     return db.Products.bulkCreate(products);
-// }).then(() => { 
-//     db.sequelize.close();
-// });
